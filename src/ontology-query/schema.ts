a031d96e8be063b050ae80dc0e3cd2a8abe47bda
lwd3737 <lwd432@gmail.com>
2025-10-24 17:38:37 +0900

    feat: query dsl - load 기본 스키마

--- src/ontology-query/schema.ts
+++ src/ontology-query/schema.ts
@@ -1,8 +1,40 @@
+import { PropertyValueType } from "@/ontology/metadata/ontology-type-schema";
+
 export interface OntologyQueryDSL {
   pipeline: PipelineStep[];
 }
 
 export interface PipelineStep {
   name: string;
-  // node
+  node: QueryNode;
 }
+
+interface SharedQueryNode<Type> {
+  type: Type;
+  objectType: string;
+  filter: QueryFilter;
+}
+
+export type QueryNode = LoadQueryNode;
+
+export interface LoadQueryNode extends SharedQueryNode<"load"> {
+  select?: string[];
+  orderBy?: {
+    fields: string[];
+  };
+}
+
+export type QueryFilter = CompareOperator;
+
+export interface CompareOperator {
+  op: "eq" | "gt" | "gte" | "lt" | "lte";
+  field: string;
+  value: ScalarType;
+}
+
+export type ScalarType =
+  | PropertyValueType.NUMBER
+  | PropertyValueType.STRING
+  | PropertyValueType.BOOLEAN
+  | PropertyValueType.DATETIME
+  | PropertyValueType.ENUM;
